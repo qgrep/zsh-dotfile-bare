43d5271e0168d9bfc54ef001832a7c8e2878b451
xmg <oremek@raphael-christopher.de>
2022-12-10 00:42:46 +0100

    ...

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+banner() {
+  printf "%s\n" \
+    '
+   @@@@@@@   @@@@@@  @@@@@@@ @@@@@@@@ @@@ @@@      @@@@@@@@  @@@@@@
+   @@!  @@@ @@!  @@@   @@!   @@!      @@! @@!      @@!      !@@    
+   @!@  !@! @!@  !@!   @!!   @!!!:!   !!@ @!!      @!!!:!    !@@!! 
+   !!:  !!! !!:  !!!   !!:   !!:      !!: !!:      !!:          !:!
+   :: :  :   : :. :     :     :       :   : ::.: : : :: ::: ::.: : 
+      '
+}
+
+install () { 
+git clone --bare https://gitlab.raphael-christopher.de/xmg/zsh-dotfile-bare.git ~/.dotfiles
+
+
+# oh my 
+banner sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# p10
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+exec zsh
+
+
+# plugins 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+}
+
+
