96bb9a01b2af51a3ea2d89939c0dd644c9c35ea7
qgrep <webmaster@raphael-christopher.de>
2022-12-14 11:35:11 +0100

    ...

--- deploy.sh
+++ deploy.sh
@@ -46,28 +46,43 @@ dot clone --bare --recurse-submodules https://github.com/qgrep/zsh-dotfile-bare.
 #------------------------------------------------------------------------------#
 # Backup already existing dotfiles
 #------------------------------------------------------------------------------#
-files=($(dot ls-tree -r HEAD | awk '{print $NF}'))
-bkp=.dotfiles.backup
-for f in "${files[@]}"; do
-  # File at root ==> back up file
-  if [[ $(basename "$f") = "$f" ]]; then
-    [[ -f "$HOME/$f" ]] && mkdir -p "$HOME/$bkp" && mv "$HOME/$f" "$HOME/$bkp" && echo "> Backing up: $f ==> $bkp/$f"
+#files=($(dot ls-tree -r HEAD | awk '{print $NF}'))
+#bkp=.dotfiles.backup
+#for f in "${files[@]}"; do
+#  # File at root ==> back up file
+#  if [[ $(basename "$f") = "$f" ]]; then
+#    [[ -f "$HOME/$f" ]] && mkdir -p "$HOME/$bkp" && mv "$HOME/$f" "$HOME/$bkp" && echo "> Backing up: $f ==> $bkp/$f"
   # File in nested directory ==> back up outermost directory
-  else
-    d=${f%%/*}
-    if [[ -d "$HOME/$d" ]]; then
-      [[ -d "$HOME/$bkp/$d" ]] && rm -rf "$HOME/$bkp/$d"
-      mkdir -p "$HOME/$bkp" && mv "$HOME/$d" "$HOME/$bkp" && echo "> Backing up: $d/ ==> $bkp/$d/"
-    fi
-  fi
-done
+#  else
+#    d=${f%%/*}
+#    if [[ -d "$HOME/$d" ]]; then
+#      [[ -d "$HOME/$bkp/$d" ]] && rm -rf "$HOME/$bkp/$d"
+#      mkdir -p "$HOME/$bkp" && mv "$HOME/$d" "$HOME/$bkp" && echo "> Backing up: $d/ ==> $bkp/$d/"
+#    fi
+#  fi
+#done
 
 #------------------------------------------------------------------------------#
 # Install
 #------------------------------------------------------------------------------#
-dot checkout
-dot submodule init
-dot submodule update
-dot config status.showUntrackedFiles no
-echo "> Success! The following dotfiles have been installed to $HOME:"
-printf '    %s\n' "${files[@]}"
+#dot checkout
+#dot submodule init
+#dot submodule update
+#dot config status.showUntrackedFiles no
+#echo "> Success! The following dotfiles have been installed to $HOME:"
+#printf '    %s\n' "${files[@]}"
+
+git clone --bare https://github.com/qgrep/zsh-dotfile-bare.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
